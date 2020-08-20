88bd7da0ec0972dad21d4ee34a0c26c088746185
Nicolas Bossard <nicolas.bossard@orange.com>
2020-08-20 13:26:09 +0200

    Removing web folder (now unused)

--- chickenweb/close_script.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "Closing chicken door...";
-$command = escapeshellcmd('/opt/chickendoor/close_door.py');
-$execres = shell_exec($command);
-echo "<pre>$execres</pre>";
-?>
--- chickenweb/open_script.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Opening chicken door...";
-$command = escapeshellcmd('/opt/chickendoor/open_door_force_short.py');
-//$command = escapeshellcmd('ls -la /opt/chickendoor');
-$execres = shell_exec($command);
-echo "<pre>$execres</pre>";
-?>
